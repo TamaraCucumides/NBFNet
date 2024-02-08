c1bd6e610f6866390c06e7147bca833c11c5ad75
Tamara Cucumides <tacucumides@uc.cl>
2024-02-09 00:17:36 +0100

    Create stupid.sh

---
+++ stupid.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --workdir=/home/tacucumides/storage
+#SBATCH --ntasks=1
+#SBATCH --job-name=nbfnet-test
+#SBATCH --nodelist=scylla
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=tacucumides@uc.cl
+#SBATCH --output=/home/tacucumides/storage/NBFNet/logs/%A.log
+#SBATCH --gres=gpu:1
+#SBATCH --cpus=2
+#SBATCH --partition=ialab-high
+
+pwd; hostname; date
+echo "Start"
+echo $(pwd)
+cd /home/tacucumides/storage
+source miniconda3/etc/profile.d/conda.sh
+conda activate nbfnet
+cd /home/tacucumides/storage/NBFNet
+python script/run.py -c config/knowledge_graph/stupid_test.yaml --checkpoint ../pesos/model_epoch_14.pth  --gpus [0] --version v1
