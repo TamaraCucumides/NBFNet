e012e303f14122c1855748d72643fcf3f7a44470
Tamara Cucumides <tacucumides@uc.cl>
2024-02-12 21:41:45 +0100

    Create visualize.sh

---
+++ visualize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --workdir=/home/tacucumides/storage
+#SBATCH --ntasks=1
+#SBATCH --job-name=nbfnet-test
+#SBATCH --nodelist=scylla
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=tacucumides@uc.cl
+#SBATCH --output=/home/tacucumides/storage/NBFNet/logs/%A.log
+#SBATCH --gres=gpu:1
+#SBATCH --cpus=2
+#SBATCH --partition=ialab-high
+
+pwd; hostname; date
+echo "Start"
+echo $(pwd)
+cd /home/tacucumides/storage
+source miniconda3/etc/profile.d/conda.sh
+conda activate nbfnet
+cd /home/tacucumides/storage/NBFNet
+python script/visualize.py -c config/knowledge_graph/fb15k237_visualize.yaml  --checkpoint ../pesos/model_epoch_14.pth  --gpus [0] --version v1
+
+
+
+
