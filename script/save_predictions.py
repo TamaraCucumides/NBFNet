e443b951a18b780c840e05151af1239188682a92
Tamara Cucumides <tacucumides@uc.cl>
2024-02-12 23:42:26 +0100

    Update save_predictions.py

--- script/save_predictions.py
+++ script/save_predictions.py
@@ -10,12 +10,14 @@ from torchdrug.utils import comm
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from nbfnet import dataset, layer, model, task, util
 
-def save_tensor(relation, preds):
+def save_tensor(relation, preds, folder="data"):
   """receives the relation number, alongside with the tensor with all predictions 
   and saves it into the respective .pt"""
 
-  
-  pass
+  if not os.path.exists(folder):
+    os.makedirs(folder)
+    pred_filename = os.path.join(folder, f'{relation}.pt')
+    torch.save(preds, pred_filename)
 
 def create_triples(relation):
   """Create all the relevant triples"""
@@ -26,7 +28,6 @@ def create_triples(relation):
     triples.append(t)
   return triples
 
-
 def obtain_results(solver, triplet):
     h, t, r = triplet.tolist()
     triplet = torch.as_tensor([[h, t, r]], device=solver.device)
@@ -36,7 +37,6 @@ def obtain_results(solver, triplet):
     #pos_pred = pred.gather(-1, target.unsqueeze(-1))
     #rankings = torch.sum((pos_pred <= pred) & mask, dim=-1) + 1
     #rankings = rankings.squeeze(0)
-  
     return pred
 
 
