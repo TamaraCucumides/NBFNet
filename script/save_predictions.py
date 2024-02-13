22c9d8826360f3bbfc0f3ce13e8071474203d162
Tamara Cucumides <tacucumides@uc.cl>
2024-02-13 01:06:46 +0100

    Update save_predictions.py

--- script/save_predictions.py
+++ script/save_predictions.py
@@ -37,7 +37,8 @@ def obtain_results(solver, triplet):
     #pos_pred = pred.gather(-1, target.unsqueeze(-1))
     #rankings = torch.sum((pos_pred <= pred) & mask, dim=-1) + 1
     #rankings = rankings.squeeze(0)
-    return pred[0][0]
+
+    return torch.round(pred[0][0] * 100).to(torch.int8)
 
 
 if __name__ == "__main__":
