87bee9943dd8fb75c7d4ea875475f25cb515f13c
Tamara Cucumides <tacucumides@uc.cl>
2024-02-14 00:55:51 +0100

    Update save.sh

--- save.sh
+++ save.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=tacucumides@uc.cl
 #SBATCH --output=/home/tacucumides/storage/NBFNet/logs/%A.log
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:0
 #SBATCH --cpus=2
 #SBATCH --partition=ialab-high
 
@@ -17,7 +17,7 @@ cd /home/tacucumides/storage
 source miniconda3/etc/profile.d/conda.sh
 conda activate nbfnet
 cd /home/tacucumides/storage/NBFNet
-python script/save_predictions.py -c config/knowledge_graph/fb15k237_visualize.yaml  --checkpoint ../pesos/model_epoch_14.pth  --gpus [0] --version v1
+python script/save_predictions.py -c config/knowledge_graph/fb15k237_visualize.yaml  --checkpoint ../pesos/model_epoch_14.pth  --gpus null --version v1
 
 
 
