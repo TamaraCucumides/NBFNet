9050099861782ece07ba4e46ec036031755b3896
Tamara Cucumides <tacucumides@uc.cl>
2024-02-20 13:27:12 +0100

    Update save.sh

--- save.sh
+++ save.sh
@@ -17,7 +17,7 @@ cd /home/tacucumides/storage
 source miniconda3/etc/profile.d/conda.sh
 conda activate nbfnet
 cd /home/tacucumides/storage/NBFNet
-python script/save_predictions.py -c config/knowledge_graph/fb15k237_visualize.yaml  --checkpoint ../pesos/model_epoch_14.pth  --gpus null --version v1
+python script/save_predictions.py -c config/knowledge_graph/fb15k237_visualize.yaml  --checkpoint ../pesos/model_epoch_14.pth  --gpus [0] --version v1
 
 
 
